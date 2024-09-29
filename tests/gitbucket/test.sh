14c343087cc95936c34e39f778ae4dd21a785e5a
Kojiro ONO <ono.kojiro@gmail.com>
2024-09-29 11:55:30 +0900

    remove unused files

--- tests/gitbucket/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl https://192.168.0.98:8443/gitbucket/
--- tests/ldaps/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-addr=`ip addr show br0 | grep 'inet ' | gawk -F' +|/' '{ print $3 }'`
-
-ldapsearch -H ldaps://$addr -D cn=Manager,dc=example,dc=com -W
-
